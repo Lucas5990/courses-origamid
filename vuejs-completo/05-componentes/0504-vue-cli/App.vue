96438e74bb4919f5c3d13e2d90c192308bb754bc
Lucas5990 <lucasandrade5990@gmail.com>
2020-02-07 16:22:17 -0300

    components

---
+++ vuejs-completo/05-componentes/0504-vue-cli/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>{{ mensagem }}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            mensagem: "Arquivo VUE"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
